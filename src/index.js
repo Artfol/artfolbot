b78ccb5356d2760e92654b4748dd5bf49f678a36
Stefan Chao <Stefan.chao@kcl.ac.uk>
2020-07-17 00:15:34 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const discord = require('discord.js');
+const client = new discord.Client({ partials: ['MESSAGE', 'REACTION']});
+const { registerCommands, registerEvents } = require('./utils/registry');
+(async () => {
+    client.login(process.env.BOT_TOKEN);
+    client.commands = new Map();
+    client.cachedMessageReactions = new Map();
+    await registerEvents(client, '../events');
+    await registerCommands(client, '../commands');
+
+})();
