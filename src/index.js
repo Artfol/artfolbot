ed55d3c40eb7025471b0742c2c02e32cebd6235f
Stefan Chao <Stefan.chao@kcl.ac.uk>
2020-07-17 00:43:19 +0100

    some commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const discord = require('discord.js');
 const client = new discord.Client({ partials: ['MESSAGE', 'REACTION']});
 const { registerCommands, registerEvents } = require('./utils/registry');
